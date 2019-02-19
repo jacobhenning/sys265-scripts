96977e537582b2d1b4f8da8581700f25d6daea88
root <root@docker01-jacob.jacob.local>
2019-02-19 14:27:21 -0500

    modified secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,12 @@
 #adds a public key to that users authorized keys file
 #removes the ability  to root ssh
 
-echo "testing 123"
+#create ssh user
+username=$1
+
+useradd ${username}
+mkdir /home/${username}/.ssh
+cp /home/jake/sys265-scripts/linux/public-keys/sys265.pub /home/${username}/.ssh/authorized_keys
+chmod 700 /home/${username}/.ssh
+chmod 600 /home/${username}/.ssh/authorized_keys
+chown -R  ${username}:${username} /home/${username}/.ssh
